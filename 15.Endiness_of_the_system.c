4f6557ccc924c8df16c07bb000b0fad4fc89d66b
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-26 18:12:20 +0530

    Update 15.Endiness_of_the_system.c

--- 15.Endiness_of_the_system.c
+++ 15.Endiness_of_the_system.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-int is_little_endian(char , unsigned int);
+int is_little_endian(char , unsigned int);  //funciton prototype
 
 int main()
 {
-  unsigned int num = 0x1;
-  char *cptr = (char*)&(num);
-  is_little_endian(*cptr, num);
+  unsigned int num = 0x1;    //variable defination 
+  char *cptr = (char*)&(num);  //variable defination
+  is_little_endian(*cptr, num); //calling fun and passing value at cptr as well as num as AP
 }
 
-int is_little_endian(char cpt, unsigned int num)
+int is_little_endian(char cpt, unsigned int num) //function def 
 {
-    if(cpt == num)
+    if(cpt == num)   //cheking if cpt eqauls to num system is liTTle endian
     {
        printf("System is little endian\n");    
     }
