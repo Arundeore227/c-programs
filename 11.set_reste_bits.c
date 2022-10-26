32774f76cb7df943d81b953a3aa337a6e71b21c2
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-26 17:58:29 +0530

    Update 11.set_reste_bits.c

--- 11.set_reste_bits.c
+++ 11.set_reste_bits.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-//#include<string.h>
 int main() {
+    //varible declaration
     int num, pos, choice;
-    printf("ENter a number\n");
-    scanf("%d", &num);
+    printf("ENter a number\n");   //mssge for the user
+    scanf("%d", &num);        //reading number from user
 
-    printf("Enter the pos\n");
-    scanf("%d", &pos);
+    printf("Enter the pos\n");   //mssge for the user
+    scanf("%d", &pos);           //reading pos form the user
 
-    printf("1: set\n2: reset(clear)\n \nENter your choice\n ");
-    scanf("%d", &choice);
+    printf("1: set\n2: reset(clear)\n \nENter your choice\n ");  //mssge for the user
+    scanf("%d", &choice);   //i have appied switch statement here for user convinenece
 
-    switch(choice)
+    switch(choice)          //switch statement to check users choice
     {
-        case 1: printf("%d", num |= (1<<pos));
+        case 1: printf("%d", num |= (1<<pos));       //logic to set bit in number using |
+        break;                                       //break is to avoid fall through
+        case 2: printf("%d", num & ~(1<<pos));       //logic to clear bit in number
         break;
-        case 2: printf("%d", num & ~(1<<pos));
-        break;
-        default :printf("Please enter valid input\n");
+        default :printf("Please enter valid input\n"); //mssge if user eneter fuzzy choice or invalid choice 
     }
 }
