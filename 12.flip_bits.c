b9d9ab3b4addbeb2cc8d0aed10d64a41c4da3bd5
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-26 18:00:22 +0530

    Update 12.flip_bits.c

--- 12.flip_bits.c
+++ 12.flip_bits.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main() {
+    //varibale declaration
     int num;
-    printf("ENter a number\n");
-    scanf("%d", &num);
-    num ^= 0xff;
-    printf("number after flipping its bit %d\n", num);
+    
+    printf("ENter a number\n");  //mssge for the user   
+    scanf("%d", &num);           //reading number from the user
+    num ^= 0xff;                 //for flippin the bits xoring num with(0xff) or (-1)
+    printf("number after flipping its bit %d\n", num);  //mssge 
 }
