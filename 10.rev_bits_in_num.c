307d639c0889b2b90be2fa77a356c905a238abeb
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-26 17:52:58 +0530

    Update 10.rev_bits_in_num.c

--- 10.rev_bits_in_num.c
+++ 10.rev_bits_in_num.c
@@ -1,15 +1,18 @@
 //reverse bits of a integer
 #include <stdio.h>
 int main() {
+    //varibale declaration
     int num, rev_bits=0;
-    printf("ENter a number\n");
-    scanf("%d", &num);
-    while(num>0)
+    
+    printf("ENter a number\n");  //mssge for the user
+    scanf("%d", &num);      //reading user input
+    
+    while(num>0)            //running loop till num greater than 0
     {
-        rev_bits = rev_bits << 1;
-        if(num & 1 == 1)
-          rev_bits = rev_bits ^ 1;
-        num = num >> 1;
+        rev_bits = rev_bits << 1;    //left shifting bit and storing res into rev_bits variable
+        if(num & 1 == 1)             //checking if num&1 = 1 if yes
+          rev_bits = rev_bits ^ 1;   //xoring bits
+        num = num >> 1;              //right shift to delete lsb(right most bit) in num
     }
-    printf("%d", rev_bits);
+    printf("%d", rev_bits);         //printing rev bit
 }
