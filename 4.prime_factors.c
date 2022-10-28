14c62babc3db774afd2d01ec2a23d90ea1e5de1f
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 15:09:26 +0530

    Update 4.prime_factors.c

--- 4.prime_factors.c
+++ 4.prime_factors.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 int main() {
+    //variable declaration
     int flag=0, num, i, j;
-    printf("ENter a number\n");
-    scanf("%d", &num);
-    for(i = 2; i <= num/2; i++)
+    printf("ENter a number\n");   //mssge for the user
+    scanf("%d", &num);   //reading input form the user
+    for(i = 2; i <= num/2; i++)  //running loop half times of the number to save iteration
     {
-        if(num % i==0)
+        if(num % i==0)  //if num is divisible by i giving zero 
         {
-          for(j=2; j<=i/2; j++)
+          for(j=2; j<=i/2; j++)  //running another loop to avoid multiple divisiors 
           {
-            if(i%j==0)
+            if(i%j==0) //checkeing if i % 2 gives zero
             {
-                flag =1;
-                break;
+                flag = 1;  //setting the flag and breaking the loop
+                break;     //this break to avaoid multiple primes
             }
           }
-          if(flag == 0)
-            printf("%d ", i);
+          if(flag == 0)  //if flag is zero
+            printf("%d ", i);  //then only printing is prime
         }
     }
 }
