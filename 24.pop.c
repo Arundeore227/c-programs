edb0ff4066e758905240c85b01deb9142cc937f8
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 13:16:46 +0530

    Update 24.pop.c

--- 24.pop.c
+++ 24.pop.c
@@ -1,21 +1,21 @@
-#include "24.stack.h"
+#include "24.stack.h"  //adding header file to subtitute everything written in stack.h
 
-int pop(S_array *s, data_t *data)
+int pop(S_array *s, data_t *data)   //function def
 {
-   if(is_empty(s->top) == STACK_EMPTY)
+   if(is_empty(s->top) == STACK_EMPTY)    //checking stack empty or not
    {
-	   return STACK_EMPTY;
+	   return STACK_EMPTY;   //if yes returning stack empty
    }
   
-  *data = s->array[s->top];
-  (s->top)--;
-   return SUCCESS;
+  *data = s->array[s->top];      //if not storing data into data var and deleting current data
+  (s->top)--;  //dec stack pointer 
+   return SUCCESS;  //returning success
 }
-int is_empty(int top)
+int is_empty(int top)   //function defination 
 {
-	if(top == -1)
+	if(top == -1)   //condition for stack empty
 	{ 
 		return STACK_EMPTY;
 	}
-	return 1;
+	return 1; //returning 1 if stack is not empty
 }
