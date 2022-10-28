cee646a0e5e55eb3cc9792e8d1389f91d4aa3c68
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 13:11:37 +0530

    Update 24.peak1.c

--- 24.peak1.c
+++ 24.peak1.c
@@ -1,11 +1,11 @@
-#include"24.stack.h"
-int peak(S_array *s, data_t *data)
+#include"24.stack.h"  //adding header file to subtitute everything written in stack.h
+int peak(S_array *s, data_t *data)  //func def
 {
-	if(is_empty(s->top) == STACK_EMPTY)
+	if(is_empty(s->top) == STACK_EMPTY)   //condition to check stack is empty or not
 	{
-		return STACK_EMPTY;
+		return STACK_EMPTY;   //if yes returning stack_empty
 	}
 
-	*data = s->array[s->top];
-	return SUCCESS;
+	*data = s->array[s->top];   //if no reading top elem data and storing into data var
+	return SUCCESS;  //returning success opr successfull
 }
