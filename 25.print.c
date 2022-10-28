8268646ea71f13eab41c00d088bd14206e1508b6
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 14:56:06 +0530

    Update 25.print.c

--- 25.print.c
+++ 25.print.c
@@ -1,17 +1,17 @@
-#include"25.queue.h"
+#include"25.queue.h"  //it will subtitue everything written in 25.queue.h
 
-void print(Q_array q)
+void print(Q_array q)   //function print is atking q as input nad returning nothing
 {
-	if(is_empty(q.front) == Q_EMPTY)
+	if(is_empty(q.front) == Q_EMPTY)  //condi to check if queue is empty or not
 	{
-		printf("Queue is empty\n");
-		return;
+		printf("Queue is empty\n");  //if yes printing queue is empty mssge
+		return;  //returning
 	}
-	printf("Elements in queue are  ");
-	while(q.front <= q.rear)
+	printf("Elements in queue are  ");   //if not printing mssge 
+	while(q.front <= q.rear)     //logic to print queue elements
 	{
 		printf("%d",q.array[q.front]);
-		(q.front)++;
+		(q.front)++;  //inc front pointer
 	}
 	putchar('\n');
 }
