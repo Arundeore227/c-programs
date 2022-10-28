ab75fdf7058ee3764d37b6df1c79ba3fb6a2daa4
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 12:48:18 +0530

    Update 18.my_atoi.c

--- 18.my_atoi.c
+++ 18.my_atoi.c
@@ -1,25 +1,29 @@
 //string to integer atoi
-#include <stdio.h>
-int my_atoi(char *);
+#include <stdio.h> 
+int my_atoi(char *);   //function declaration
 int main()
 {
-   char str[100];
-   printf("Enter a number string\n");
-   scanf("%[^\n]", str);
-   printf("string to int is %d", my_atoi(str));
+   //var declaration
+   char str[100];  
+   printf("Enter a number string\n");    //mssge for the user
+   scanf("%[^\n]", str);                 //reading string from the user
+   printf("string to int is %d", my_atoi(str)); //calling fun and print func returned res 
     return 0;
 }
 
-int my_atoi(char *str)
+int my_atoi(char *str)  //function defination of my_atoi
 {
+   //var declaration
     int i=0, to_int=0;
+   
+   //running loop till last NULL
     while(str[i])
     {
-        if(str[i] >= '0' && str[i] <= '9')
+        if(str[i] >= '0' && str[i] <= '9')        //checking if current str index value(elem) is between '0' to '9' or not if yes
         {
-            to_int = to_int * 10 + (str[i] - '0');
+            to_int = to_int * 10 + (str[i] - '0');   //converting it into int
         }
-        i++;
+        i++;  //incremeting loop counter
     }
-    return to_int;
+    return to_int;   //returning int eq or string
 }
