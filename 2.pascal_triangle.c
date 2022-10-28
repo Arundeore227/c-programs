2bbf7429185ea74a3ee681397b590411bcfa9cf8
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 12:51:43 +0530

    Update 2.pascal_triangle.c

--- 2.pascal_triangle.c
+++ 2.pascal_triangle.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 int main() {
-
+    
+    //variable declaration
     int n, ncr, i, j, k;
-    printf("ENter a num\n");
-    scanf("%d", &n);
+    printf("ENter a num\n");   //mssge for the user
+    scanf("%d", &n);           //reading input from the user
 
-    for(i=0; i<n; i++)
+    for(i=0; i<n; i++)     //ruuning nested loop till n
     {
         for(j=1; j<n-i; j++)
         {
-            printf(" ");
+            printf(" ");     //logic to print spaces
         }
-        for(k=0; k<=i; k++)
+        for(k=0; k<=i; k++)   
         {
             if(i==0 || k==0)
             {
                 ncr = 1;
-                printf("%d ", ncr);
+                printf("%d ",  ncr);  //logic for printing first and last digit in triangle
             }
             else
             {
-                ncr = ncr *(i-k+1)/k;
+                ncr = ncr *(i-k+1)/k;   //logic to print middle all elems expect first and last
                 printf("%d ", ncr);
             }
         }
-        printf("\n");
+        printf("\n");  //printing to new line
     }
 }
