a5e7255ea16d2ad3b1419cdb4d21bf93d0da2ff1
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 15:24:56 +0530

    Update 6.lcm_gcm.c

--- 6.lcm_gcm.c
+++ 6.lcm_gcm.c
@@ -1,33 +1,35 @@
 #include<stdio.h>
-int my_gcd(int,int);
-int my_lcm(int,int);
+int my_gcd(int,int); //function declaration
+int my_lcm(int,int); //function declaration
 void main()
 {
+	//variable decalaration
 	int num1,num2;
-	printf("enter 2 num numbers\n");
-	scanf("%d %d",&num1,&num2);
-	printf("the gcd and lcm of given number is = %d %d\n", my_gcd(num1, num2), my_lcm(num1, num2));
+	printf("enter 2 num numbers\n");     //mssge for the suer
+	scanf("%d %d",&num1,&num2);          //reading num1 and num2 from the user
+	printf("the gcd and lcm of given number is = %d %d\n", my_gcd(num1, num2), my_lcm(num1, num2)); //calling my_gcd and my_lcm funciton and printion res
 }
 
-int my_gcd(int n1,int n2)
+int my_gcd(int n1,int n2)     //function def of my_gcd
 {
-	if(n1==0)
+	if(n1==0)  //base codition control recursion
 		return n2;
-	if(n2==0)
+	if(n2==0)  //base codition control recursion
 		return n1;
-	return my_gcd(n2,n1%n2);
+	return my_gcd(n2,n1%n2); //base codition control recursion
 }
 
-int my_lcm(int n1,int n2)
+int my_lcm(int n1,int n2)  //function def of my_lcm
 {
-	int dif;
-	dif=(n1>n2)?n1:n2;
-	while (1)
+	//variable declaration
+	int dif; 
+	dif=(n1>n2)?n1:n2;  //condition  to find largest between two using ternery operator
+	while (1)  //infinite loop
 	{
-		if((dif%n1==0) && (dif%n2==0))
+		if((dif%n1==0) && (dif%n2==0))   //checking condition if true
 		{
-			return dif;break;
+			return dif;break;  //returing diff and breaking the loop
 		}
-		dif++;
+		dif++;  //if not incrementing diff
 	}
 }
