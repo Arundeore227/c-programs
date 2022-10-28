6d0e2f8b3bd889904127a3073ff2d5bfb8e3a1c0
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 15:32:27 +0530

    Update 9.higest_freq_in_str.c

--- 9.higest_freq_in_str.c
+++ 9.higest_freq_in_str.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 int main() {
+    //varible declaartion
     int i, j, cnt=0, max_count=0;
     char str[100], rep_char;
-    printf("ENter a string\n");
-    scanf("%[^\n]", str);
-    for(i=0; str[i]; i++)
+    printf("ENter a string\n");  //mssge for the user
+    scanf("%[^\n]", str); //reading string from the user
+    for(i=0; str[i]; i++)  //travesing over the string
     {
-        cnt = 0;
-        for(j=0; str[j]; j++)
+        cnt = 0;    //ressting count to 0 
+        for(j=0; str[j]; j++)  //tarversing inner loop on string
         {
-            if(str[i] == str[j])
+            if(str[i] == str[j])  //if str[i] mateches with str[j] 
             {
-                cnt++;
+                cnt++;     //incrementing the count
             }
         }
-        if(max_count < cnt)
+        if(max_count < cnt)  //logic to find larest
             {
               max_count = cnt;
-              rep_char = str[i];
+              rep_char = str[i];  //storing largest occured char
             }
     }
-    if(max_count>1)
+    if(max_count>1)  //condition to check if char repeted more than one or not
     printf("%c is max repeating char repeted %d time", rep_char, max_count);
     else
        printf("Every character repeted once\n");
