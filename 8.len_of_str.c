e97506a3bddc7d874279c00115af16623fc9d9ea
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 15:28:49 +0530

    Update 8.len_of_str.c

--- 8.len_of_str.c
+++ 8.len_of_str.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 //#include<string.h>
 int main() {
+    //var declaration
     char str[100];
-    printf("ENter the string whose lenght you wnat to calculate\n");
-    scanf("%[^\n]", str);
+    printf("ENter the string whose lenght you wnat to calculate\n"); //mssge for the user
+    scanf("%[^\n]", str); //reading string from the user
+    //var declaration
     int len=0;
-    while(str[++len]);
-    printf("len of entred string is %d\n", len);
+    while(str[++len]);  //logic to count len of the string
+    printf("len of entred string is %d\n", len); //printing len of the string
    // printf("len of entred string is %d", strlen(str));
     return 0;
 }
