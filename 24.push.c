d41e40dce041c0851e915587da01612cd0a0c4f5
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 13:26:36 +0530

    Update 24.push.c

--- 24.push.c
+++ 24.push.c
@@ -1,20 +1,20 @@
-#include "24.stack.h"
+#include "24.stack.h"     //adding header file to subtitute everything written in stack.h
 
-int push(S_array *s, data_t data)
+int push(S_array *s, data_t data)   //fucntion def
 {
-   if(is_full(s->top) == STACK_FULL)
+   if(is_full(s->top) == STACK_FULL)    //condition to check stack full
    {
-	   return STACK_FULL;
+	   return STACK_FULL;  //if full returing stack full
    }
-   (s->top)++;
-   s->array[s->top] = data;
-   return SUCCESS;
+   (s->top)++;  //if not incrementing stack pointer 
+   s->array[s->top] = data;  //adding data 
+   return SUCCESS;   //returing success
 }
-int is_full(int top)
+int is_full(int top)   //function to check stack is full or not
 {
-	if(top == MAX-1)
+	if(top == MAX-1)   //to stack full
 	{ 
-		return STACK_FULL;
+		return STACK_FULL; //yes retruing stack full
 	}
 	return 1;
 }
