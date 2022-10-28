570eb4423f18a87514082588d1df6b331208b5bf
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 13:08:06 +0530

    Update 23.duplicate.c

--- 23.duplicate.c
+++ 23.duplicate.c
@@ -2,31 +2,30 @@
 #include<string.h>
 int main()
 {
+	//varibale declaration
 	char str[100];
 	int i, j;
 
-	printf("ENter a string\n");
-	scanf("%[^\n]", str);
+	printf("ENter a string\n");    //mssge for the user
+	scanf("%[^\n]", str);          //reading string from the user   
 
-	printf("%ld\n", strlen(str));
-
-	for(i=0; i < strlen(str); i++)
+	for(i=0; i < strlen(str); i++)    //running outer loop till len 
 	{
-		for(j=i+1; j < strlen(str);)
+		for(j=i+1; j < strlen(str);)   //running inner loop till len of str
 		{
-			if(str[i] == str[j])
+			if(str[i] == str[j])       //cond to check duplicate elem in loop
 			{
-				for(int k=j; str[k]; k++)
+				for(int k=j; str[k]; k++)       //if true running one loop assiging current index to k and running loop till last char
 				{ 
-					str[k] = str[k+1];
+					str[k] = str[k+1];   //logic to delete elem in string
 				}
 			}
 			else 
 			{
-				j++;
+				j++;   //inc j inner loop counter
 			}
 
 		}
 	}
-	printf("%s\n", str);
+	printf("%s\n", str);  //printing string
 }
