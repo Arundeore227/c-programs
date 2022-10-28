bf67fc07f88bb803628429aac55da87942ef4c91
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 13:14:02 +0530

    Update 24.peep.c

--- 24.peep.c
+++ 24.peep.c
@@ -1,15 +1,15 @@
-#include"24.stack.h"
+#include"24.stack.h"   //adding header file to subtitute everything written in stack.h
 void peep(S_array s)
 {
-	if(is_empty(s.top) == STACK_EMPTY)
+	if(is_empty(s.top) == STACK_EMPTY)   //condition to check stack empty or not
 	{
           printf("Stack is empty\n");
 	}
-	printf("Stack elements are:- ");
-	while(s.top > -1)
+	printf("Stack elements are:- ");     
+	while(s.top > -1)  //condition to print stack elem if its greter than index -1
 	{
-		printf("%d -> ", s.array[s.top]);
-		(s.top)--;
+		printf("%d -> ", s.array[s.top]);   //printing stack elem
+		(s.top)--;   //decrementing top pointer 
 	}
-	putchar('\n');
+	putchar('\n');  //to print new line
 }
