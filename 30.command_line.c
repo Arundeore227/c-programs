bb0a6062bfe772df5a44b722439f8f958b272fd8
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 15:03:49 +0530

    Update 30.command_line.c

--- 30.command_line.c
+++ 30.command_line.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
-int main(int argc, char **argv)
+int main(int argc, char **argv)  //readig command line args from the user
 {
+	//variable declaration
 	int i;
-	printf("count of total args %d\n", argc);
+	printf("count of total args %d\n", argc);  //mssge for the user
 
 	printf("arguments are : \n");
 
 	for(i=0; i<argc; i++)
 	{
-		printf("%s ", argv[i]);
+		printf("%s ", argv[i]);   //logic to print command line passed args
 	}
 	printf("\n");
 }
