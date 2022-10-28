84e5f29e0762960c8132f721a41ffd7153b15dcb
Arundeore227 <116165860+Arundeore227@users.noreply.github.com>
2022-10-28 15:02:14 +0530

    Update 3.matrix_transpose.c

--- 3.matrix_transpose.c
+++ 3.matrix_transpose.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
 int main() {
+    
+    //varible declaration
     int row , col, i, j;
-    printf("Enter row and col for matrix array\n");
-    scanf("%d %d", &row, &col);
+    printf("Enter row and col for matrix array\n");  //mssge for the user
+    scanf("%d %d", &row, &col);   //reading col and rows from the user
 
-    int arr[row][col], transpose[row][col];
-    printf("Enter elemets for %d x %d matrix\n", row,col);
+    int arr[row][col], transpose[row][col];  //declaring array of size colxrow 
+    printf("Enter elemets for %d x %d matrix\n", row,col); //mssge for  the user
+    
     for(i = 0; i< row; i++)
     {
         for(j=0; j<col; j++)
         {
-            scanf("%d", &arr[i][j]);
+            scanf("%d", &arr[i][j]);       //reading matrix input from the user
         }
     }
 
-    for(i = 0; i< row; i++)
+    for(i = 0; i < row; i++)
     {
         for(j=0; j<col; j++)
         {
-            transpose[j][i] = arr[i][j];
+            transpose[j][i] = arr[i][j];   //logic of transposing matrix
         }
     }
     printf("\n\n");
@@ -27,7 +30,7 @@ int main() {
     {
         for(j=0; j<col; j++)
         {
-            printf("%d ", transpose[i][j]);
+            printf("%d ", transpose[i][j]);    //printing transposed matrix
         }
         printf("\n");
     }
